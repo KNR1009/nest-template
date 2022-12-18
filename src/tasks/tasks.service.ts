0a4e5cbadb816fc5d604cbf3ceb87e16c518d60b
伊東和成 <itokazunari5375@icloud.com>
2022-12-18 19:29:46 +0900

    feat: 新規作成・取得

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,4 +1,27 @@
 import { Injectable } from '@nestjs/common';
+import { Task } from '@prisma/client';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { CreateTaskDto } from './dto/create-task.dto';
 
 @Injectable()
-export class TasksService {}
+export class TasksService {
+  constructor(private readonly prisma: PrismaService) {}
+  async getTasks(): Promise<Task[]> {
+    return await this.prisma.task.findMany();
+  }
+
+  async getTask(id: number): Promise<Task> {
+    return await this.prisma.task.findFirst({
+      where: {
+        id: id,
+      },
+    });
+  }
+  async createTask(dto: CreateTaskDto) {
+    await this.prisma.task.create({
+      data: {
+        ...dto,
+      },
+    });
+  }
+}
