b0375074461fbf04c7a57ba4ae8d2bbb681edf5f
伊東和成 <itokazunari5375@icloud.com>
2022-12-18 18:42:06 +0900

    fix: 命名を変更

--- prisma/migrations/20221218085405_/migration.sql
+++ prisma/migrations/20221218085405_/migration.sql
@@ -1,8 +1,8 @@
 -- CreateTable
-CREATE TABLE "User" (
+CREATE TABLE "Task" (
     "id" SERIAL NOT NULL,
     "name" TEXT NOT NULL,
     "isCompleted" BOOLEAN NOT NULL DEFAULT false,
 
-    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+    CONSTRAINT "Task_pkey" PRIMARY KEY ("id")
 );
