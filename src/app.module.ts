17018493272c6c9b647ade9d0fd0259acb59051a
伊東和成 <itokazunari5375@icloud.com>
2022-12-18 19:02:41 +0900

    add: configを追加

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
 import { TasksModule } from './tasks/tasks.module';
 import { PrismaModule } from './prisma/prisma.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [TasksModule, PrismaModule],
+  imports: [
+    TasksModule,
+    PrismaModule,
+    ConfigModule.forRoot({ isGlobal: true }),
+  ],
   controllers: [],
   providers: [],
 })
